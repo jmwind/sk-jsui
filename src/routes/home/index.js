63c64c58a8efc2b3f9834aabc4ef91e61ded0edb
Jean-Michel Lemieux <jmwind@gmail.com>
2022-08-25 21:48:37 -0400

    metric path constants

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -1,7 +1,7 @@
 import { useState } from "preact/hooks";
 import style from './style.css';
 import Metric from '../../components/metric/metric';
-import { SkData, SkConversions } from "sk-jsclient/sk-data";
+import { SkData } from "sk-jsclient/sk-data";
 
 const Home = (props) => {
 	const [metrics, setMetrics] = useState(props.metrics);
@@ -9,19 +9,19 @@ const Home = (props) => {
 	return (
 		<div class={style.home}>
 			<div class={style.left}>
-				<Metric metrics={metrics} metric_name='navigation.polarSpeedRatio' large={true} />
+				<Metric metrics={metrics} metric_name={SkData.POLAR_RATIO} large={true} />
 			</div>
 			<div class={style.right}>
-				<Metric metrics={metrics} metric_name='navigation.speedOverGround' large={false} />
+				<Metric metrics={metrics} metric_name={SkData.SOG} large={false} />
 			</div>
 			<div class={style.right}>
-				<Metric metrics={metrics} metric_name='navigation.polarSpeedTarget' large={false} />
+				<Metric metrics={metrics} metric_name={SkData.POLAR_TARGET} large={false} />
 			</div>
 			<div class={style.right}>
-				<Metric metrics={metrics} metric_name='environment.wind.angleApparent' large={false} />
+				<Metric metrics={metrics} metric_name={SkData.AWA} large={false} />
 			</div>
 			<div class={style.right}>
-				<Metric metrics={metrics} metric_name='environment.wind.speedApparent' large={false} />
+				<Metric metrics={metrics} metric_name={SkData.AWS} large={false} />
 			</div>
 		</div >
 	);
